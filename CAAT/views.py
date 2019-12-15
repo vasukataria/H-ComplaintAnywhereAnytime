63f7c5408971b6780141a9de6e8eab5ecf42e469
anunayasri <anunayasri@gmail.com>
2019-12-16 01:37:10 +0530

    Fix sign up

--- CAAT/views.py
+++ CAAT/views.py
@@ -31,8 +31,8 @@ def signUp(request):
         #gender = request.POST['gender']
         Gender = request.POST['Gender']
         Hostel = request.POST['Hostel']
-        Floor  = request.Post['Floor']
-        Room_number = request.Post['Room_number']
+        Floor  = request.POST['Floor']
+        Room_number = request.POST['Room_number']
         phone_number = request.POST['phone_number']
 
         #check pass
@@ -51,21 +51,21 @@ def signUp(request):
                     password=password1,
                     email=email,
                     first_name=first_name,
-                    last_name=last_name,
+                    last_name=last_name
+                )
+                user.save()
+
+                Profile.objects.create(
+                    user=user,
+                    type='STUDENT',
+                    dob=bday,
                     Gender = Gender,
                     Hostel = Hostel,
                     Floor = Floor,
                     Room_number = Room_number,
-                    
-
+                    phone_number = phone_number
                 )
-            user.save()
-            Profile.objects.create(user=user, type='STUDENT',dob=bday)
-            Profile.objects.create(user=user, type='STUDENT',Hostel=Hostel)
-            Profile.objects.create(user=user, type='STUDENT',Floor=Floor)
-            Profile.objects.create(user=user, type='STUDENT',Room_number=Room_number)
-            Profile.objects.create(user=user, type='STUDENT',Gender=Gender)
-            Profile.objects.create(user=user, type='STUDENT',Gender=phone_number)
+                    
             print('user created')
             return redirect('login')
         else:
